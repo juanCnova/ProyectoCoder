28780c04ef53fa2efc938e38a75302553f5f3050
juanc novakovich <juancruz.nova123@gmail.com>
2023-02-24 09:55:02 -0300

    primer commit de proyecto appCoder

---
+++ appCoder/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=20)
+    comision = models.IntegerField()
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    mail = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    mail = models.EmailField()
+    Profesion = models.CharField(max_length=20)
+
+class Entegable(models.Model):
+    nombre = models.CharField(max_length=20)
+    fecha = models.IntegerField()
+    entregado = models.BooleanField()
+
+class Tutor(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    mail = models.EmailField()
+    Profesion = models.CharField(max_length=20)
